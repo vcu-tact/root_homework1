aafd1b07b7b33f77db47146a103d57edc8b7bbe2
yourusername <tact@vcu.edu>
2021-02-09 22:29:12 -0500

    made changes to return_value

--- homework1/hw1.py
+++ homework1/hw1.py
@@ -34,5 +34,5 @@ def return_without_starting_ending_whitespace(input_string):
 def return_addition(first_number, second_number):
     """ Return the two numbers added together. """
 
-    return_value = 2+3
+    return_value = first_number + second_number
     return return_value
