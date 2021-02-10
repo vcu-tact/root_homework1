7d215fcd4a8f75339164cfb5c56167fdafd95516
yourusername <tact@vcu.edu>
2021-02-09 22:21:54 -0500

    made some changes

--- homework1/hw1.py
+++ homework1/hw1.py
@@ -4,14 +4,14 @@
 def return_number_3():
     """ This function should return an integer with the value of 3"""
 
-    return_value = None
+    return_value = 3
     return return_value
 
 
 def return_string_vcu():
     """ This function should return a string with the lowercase value of vcu"""
 
-    return_value = None
+    return_value = "vcu"
     return return_value
 
 
@@ -20,7 +20,7 @@ def return_lowercased_string(input_string):
     Return it but the lowercase version of it."""
 
     return_value = input_string
-    return return_value
+    return return_value.lower()
 
 
 def return_without_starting_ending_whitespace(input_string):
@@ -28,11 +28,11 @@ def return_without_starting_ending_whitespace(input_string):
     Return it but with the surrounding (left and right) whitespace stripped."""
 
     return_value = input_string
-    return return_value
+    return return_value.strip()
 
 
 def return_addition(first_number, second_number):
     """ Return the two numbers added together. """
 
-    return_value = None
+    return_value = 2+3
     return return_value
